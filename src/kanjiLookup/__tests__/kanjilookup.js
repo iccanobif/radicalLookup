3c4a1768264ee0d361d212b2a88652e3a87ddb68
iccanobif <iccanobif@420blaze.it>
2019-04-21 17:25:31 +0200

    lookup by radicals seems to work

--- src/kanjiLookup/__tests__/kanjilookup.js
+++ src/kanjiLookup/__tests__/kanjilookup.js
@@ -1,5 +1,7 @@
 import { radicalToKanji } from "../radicalToKanji.js";
 import { radicalDescriptions } from "../radicalDescriptions.js";
+const rewire = require("rewire");
+const kanjiLookup = rewire("../kanjilookup.js");
 
 test("radicalToKanji", () =>
 {
@@ -11,3 +13,21 @@ test("radicalDescriptions", () =>
 {
   expect(radicalDescriptions).toContainEqual({ "radical": "宀", "descriptions": "roof" })
 })
+
+test("getKanjiFromRadicalName", () =>
+{
+  const getKanjiFromRadicalName = kanjiLookup.__get__("getKanjiFromRadicalName");
+  const womanKanjiList = getKanjiFromRadicalName("woman")
+  expect(womanKanjiList).toContain("女")
+  expect(womanKanjiList).toContain("嫁")
+  expect(womanKanjiList).not.toContain("口")
+})
+
+test("getKanjiFromRadicalNames", () =>
+{
+  const womanRoofKanjiList = kanjiLookup.getKanjiFromRadicalNames(["woman", "roof"])
+  expect(womanRoofKanjiList).toContain("家")
+  expect(womanRoofKanjiList).toContain("嫁")
+  expect(womanRoofKanjiList).not.toContain("人")
+})
+
--- src/kanjiLookup/kanjilookup.js
+++ src/kanjiLookup/kanjilookup.js
@@ -1,18 +1,45 @@
 const radicalToKanji = require("./radicalToKanji.js").radicalToKanji
 const radicalDescriptions = require("./radicalDescriptions.js").radicalDescriptions
 
+// This can be moved to a utility library
+Array.prototype.uniq = function ()
+{
+    return this
+        .sort()
+        .reduce((acc, val) =>
+        {
+            if (acc[acc.length - 1] != val)
+                acc.push(val)
+            return acc
+        }, [])
+}
+
+const getKanjiFromRadicalName = (radicalName) =>
+{
+    return radicalDescriptions
+        .filter(x => x.descriptions.indexOf(radicalName) >= 0)
+        .map(x => radicalToKanji[x.radical])
+        .flat()
+        .sort()
+        .reduce((acc, val) =>
+        {
+            if (acc[acc.length - 1] != val)
+                acc.push(val)
+            return acc
+        }, [])
+}
 
 // radicalNames must be a list of names of radicals
 module.exports.getKanjiFromRadicalNames = (radicalNames) =>
 {
-
+    return radicalNames
+        .map(name => getKanjiFromRadicalName(name))
+        .flat()
+        .sort()
+        .reduce((acc, val) =>
+        {
+            if (acc[acc.length - 1] != val)
+                acc.push(val)
+            return acc
+        }, [])
 }
-
-
-module.exports.getKanjiFromRadicalName = (radicalName) =>
-{
-
-}
-
-// console.log(radicalToKanji["金"])
-// console.log(radicalDescriptions[0])
